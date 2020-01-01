5227e2ba1391c9e099d938f293c8c169ad7ba95b
ESA <firesun.protection@outlook.com>
2020-01-01 14:03:42 +0100

    LICENSE active-ending.animation

---
+++ Alexa LED Animation Files for AVS/google home.py
@@ -0,0 +1,4 @@
+  File "/private/var/mobile/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents//Untitled.py", line 2
+    __builtins__'shp:Explosion04'
+                                ^
+SyntaxError: invalid syntax
